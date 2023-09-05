2d8fdbc13898cba391106a1db7d693f22e7d45e4
Səmədov İsmət <ismetsemedov@gmail.com>
2023-09-05 14:52:30 +0400

    check

--- fullhdfilm/fullhdfilm/spiders/content.py
+++ fullhdfilm/fullhdfilm/spiders/content.py
@@ -8,5 +8,14 @@ class ContentSpider(scrapy.Spider):
 
     def parse(self, response):
         yield {
-            'href':response.css('div.izle-titles a::attr(href)').get()
+            'name': response.css('.izle-titles h1 a::text').get(),
+            'href': response.css('div.izle-titles a::attr(href)').get(),
+            'imdb': response.css('.header-sag .imdb-ic span::text').get(),
+            'score': response.css('.header-sag .puanx-ic .puanx-puan::text').getall()[1],
+            'description': response.css('div.film-ozeti div.ozet-ic p::text').getall(),
+            'director': response.css('div.dd a span::text').get(),
+            'actors': response.css('div.dd a span::text').getall(),
+            'category': response.css('div.dd a.category::text').get(),
+            'genres': response.css('div.dd a[rel="category tag"]::text').getall(),
+            'tags':response.css('li span.dt ~ div.sag.etiket::text').getall()
         }
